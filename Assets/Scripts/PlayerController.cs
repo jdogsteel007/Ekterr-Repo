9d5117d5c46d515687edc576d9bb0ec0e68dbcf0
Joe Mazero <jmazero@umail.iu.edu>
2018-02-08 09:19:56 -0500

    2
    2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private Animator anim;
+
+    private bool playerMoving;
+    //basically two different variables: lastMoveX and lastMoveY vv
+    private Vector2 lastMove;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerMoving = false;
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerMoving = true;
+            //lastMove gets the value of the horizontal axis and keeps it instead of it returning to zero
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < -0.5) {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+        else if (Input.GetAxisRaw("Vertical") < -0.5)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+
+    }
+}
