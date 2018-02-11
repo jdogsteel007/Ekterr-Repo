7ec8a3db9c0ba4ae2265580d528bab4aa826dfdb
Joe Mazero <jmazero@umail.iu.edu>
2018-02-11 17:52:09 -0500

    follow script
    cub now follows... kind of i will fix it later

---
+++ Assets/Scripts/cubController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubController : MonoBehaviour {
+
+    public GameObject player;
+    Vector3 buffer = new Vector3(-.1f, 2f, 0f);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = Vector3.Lerp(transform.position, player.transform.position - buffer, .04f);
+
+    }
+}
