af3ffb325cbd6770db108075e850dd837eaa50ca
Joe Mazero <jmazero@umail.iu.edu>
2018-02-14 10:18:52 -0500

    beams

---
+++ Assets/Scripts/playerProjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerProjectile : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject playerProj;
+    Rigidbody2D myRigid;
+    public float strength;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var go = Instantiate(playerProj, transform.position, Quaternion.identity) as GameObject;
+            myRigid =  go.GetComponent<Rigidbody2D>();
+
+            Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+            Vector3 dir = (Input.mousePosition - sp).normalized;
+            myRigid.AddForce(dir * strength);
+        }
+
+    }
+}
