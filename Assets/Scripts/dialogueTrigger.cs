8bd58815e1507b6d2e64572bcd89b0e65e8ce267
Joe Mazero <jmazero@umail.iu.edu>
2018-02-15 18:14:13 -0500

    dialogue
    started on dialogue system

---
+++ Assets/Scripts/dialogueTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueTrigger : MonoBehaviour {
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue() {
+
+
+        FindObjectOfType<dialogueManager>().StartDialogue(dialogue);
+
+    }
+
+}
---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+    public GameObject player;
+    public bool isChasing = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(isChasing == true)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.transform.position, .045f);
+        }
+        
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "player"){
+            healthBarScript.health -= 10f;
+            Debug.Log("hit");
+        }
+        
+
+        
+    }
+
+}
