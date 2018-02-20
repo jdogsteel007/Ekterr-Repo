ab4a46fcc38bad4e3eeae267be4c0c5ca5cb9165
Joe Mazero <jmazero@umail.iu.edu>
2018-02-20 11:23:19 -0500

    chase scene

---
+++ Assets/Scripts/boulderScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boulderScript : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Physics2D.IgnoreCollision(player.GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>());
+    }
+
+}
---
+++ Assets/Scripts/truckScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class truckScript : MonoBehaviour {
+
+    public float speed = .2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(new Vector3(0, speed, 0));
+
+	}
+}
