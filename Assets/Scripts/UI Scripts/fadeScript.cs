d84c40ed7b7f2a93f139f414cb2fb43b2e86c595
Joe Mazero <jmazero@umail.iu.edu>
2018-03-01 18:54:36 -0500

    tweaks

---
+++ Assets/Scripts/UI Scripts/fadeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fadeScript : MonoBehaviour {
+
+    public void FadeMe() {
+        StartCoroutine(DoFade());
+    }
+
+    IEnumerator DoFade() {
+        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+        while (canvasGroup.alpha > 0) {
+            canvasGroup.alpha -= Time.deltaTime / 2;
+            yield return null;
+        }
+        canvasGroup.interactable = false;
+        yield return null;
+    }
+
+}
---
+++ Assets/Scripts/UI Scripts/textManagerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textManagerScript : MonoBehaviour {
+
+    public GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+
+        fadeScript myFadeScript = canvas.GetComponent<fadeScript>();
+
+        myFadeScript.FadeMe();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
