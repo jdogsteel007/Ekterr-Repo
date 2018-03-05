368534254c5cc0118712fa5d825835c50d7aa457
Joe Mazero <jmazero@umail.iu.edu>
2018-03-05 14:48:32 -0500

    intro and first cut scene started

--- Assets/Scripts/UI Scripts/fadeScript.cs
+++ Assets/Scripts/UI Scripts/fadeScript.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class fadeScript : MonoBehaviour {
 
+    public float alpha;
+
     public void FadeMe() {
         StartCoroutine(DoFade());
     }
 
+    public void undoFadeMe()
+    {
+        StartCoroutine(UndoFade());
+    }
+
     IEnumerator DoFade() {
+
+
+        Canvas canvas = GetComponent<Canvas>();
+
+        if (canvas.tag == "secondText") {
+            canvas.GetComponent<CanvasGroup>().alpha = .99f;
+        }
+
         CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
         while (canvasGroup.alpha <= 1) {
             canvasGroup.alpha += Time.deltaTime * .35f;
@@ -16,6 +31,24 @@ public class fadeScript : MonoBehaviour {
         }
         canvasGroup.interactable = false;
         yield return null;
+    }
+
+    IEnumerator UndoFade()
+    {
+        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= Time.deltaTime * .012f;
+            yield return null;
+        }
+        canvasGroup.interactable = false;
+        yield return null;
+    }
+
+    private void Update()
+    {
+        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+        alpha = canvasGroup.alpha;
     }
 
 }
--- Assets/Scripts/UI Scripts/textManagerScript.cs
+++ Assets/Scripts/UI Scripts/textManagerScript.cs
@@ -5,18 +5,49 @@ using UnityEngine;
 public class textManagerScript : MonoBehaviour {
 
     public GameObject canvas;
+    public float waitFor = 0;
+    public float waitForDissolve;
 
 	// Use this for initialization
 	void Start () {
 
-        fadeScript myFadeScript = canvas.GetComponent<fadeScript>();
 
-        myFadeScript.FadeMe();
+        StartCoroutine("myDelay");
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        fadeScript myFadeScript = canvas.GetComponent<fadeScript>();
+
+        if (myFadeScript.alpha >= 1)
+        {
+            StopCoroutine("myDelay");
+            StartCoroutine("secondDelay");
+        }
+    }
+
+    IEnumerator myDelay() {
+
+
+        yield return new WaitForSeconds(waitFor);
+
+        fadeScript myFadeScript = canvas.GetComponent<fadeScript>();
+        myFadeScript.FadeMe();
+
+    }
+
+    IEnumerator secondDelay()
+    {
+
+
+        yield return new WaitForSeconds(waitForDissolve);
+
+        fadeScript myFadeScript = canvas.GetComponent<fadeScript>();
+        myFadeScript.undoFadeMe();
+
+    }
+
+
+
 }
