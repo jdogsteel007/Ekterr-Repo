06646cdf93a36eae65abe323807f88a94bf14d2e
Joe Mazero <jmazero@umail.iu.edu>
2018-03-06 19:45:47 -0500

    tweaks

--- Assets/Scripts/timedTransition.cs
+++ Assets/Scripts/timedTransition.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class timedTransition : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
+        StartCoroutine(delayedStart());
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+
+
 	}
+
+    IEnumerator delayedStart() {
+
+        yield return new WaitForSeconds(23);
+
+        SceneManager.LoadScene("lvl_01");
+
+
+    }
+
 }
