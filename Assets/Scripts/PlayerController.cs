a533b084349937506eb05d72862b94091d9083dd
DevinGoodall <Whitemansoft@gmail.com>
2018-03-27 20:38:55 -0400

    Changes
    Default bullet prefab
    updated bullet script
    Combat entity base class
    globals class
    static helper (mostly math stuff)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : CombatEntity {  //Inherets from CombatEntity so we have health and bullet handling
 
     public float moveSpeed;
 
@@ -10,22 +10,32 @@ public class PlayerController : MonoBehaviour {
 
     private bool playerMoving;
     //basically two different variables: lastMoveX and lastMoveY vv
-    private Vector2 lastMove;
+    public Vector2 LastMove;
 
     private Rigidbody2D myRigidbody;
 
 	// Use this for initialization
 	void Start () {
-
         anim = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
-
+        Health = MaxHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         playerMoving = false;
+
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));   //(Devin) here is a cleaner way of doing player movement, I hope you don't mind...
+        if (movement.magnitude > 0.5)
+        {
+            playerMoving = true;
+            myRigidbody.velocity = movement.normalized * moveSpeed;
+            LastMove = movement.normalized;
+        }
+        else myRigidbody.velocity = Vector2.zero;
+
+        /*
 
         if (Input.GetAxisRaw("Horizontal") > 0.5)
         {
@@ -67,13 +77,31 @@ public class PlayerController : MonoBehaviour {
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
         }
 
+        */
+
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
         anim.SetBool("PlayerMoving", playerMoving);
-        anim.SetFloat("LastMoveX", lastMove.x);
-        anim.SetFloat("LastMoveY", lastMove.y);
+        anim.SetFloat("LastMoveX", LastMove.x);
+        anim.SetFloat("LastMoveY", LastMove.y);
 
     }
 
+    public override void HandleBullet(Bullet bullet)
+    {
+        if (bullet.FriendlyBullet != IsFriendly)
+            Health -= bullet.Damage;
+        if (Health == 0)
+        {
+            OnKill.Invoke();
+            Kill();
+        }
+    }
+
+    public void Kill()
+    {
+        Destroy(gameObject); //RIP!
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
 
 }
